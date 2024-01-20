bd433b2a9b288bdd91cd514fcbf3e347a7f06245
KenAKAFrosty <thefrostyypaws@gmail.com>
2024-01-20 16:02:40 -0600

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
 
+export const test = server$(async function(message: string) { 
+  console.log('test', message);
+  return "hey  " + message;
+})
 export default component$(() => {
   return (
     <>
+    <button onClick$={()=> { 
+      test('test').then(console.log)
+    }}>
+      Test
+    </button>
       <h1>Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
